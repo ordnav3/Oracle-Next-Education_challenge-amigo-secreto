237a6ed592278b096d360e75d5d20accd693a8d7
Evandro Santos Afonso <evandrosantosafonso@live.com>
2025-02-07 00:20:56 -0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const amigo = document.querySelector('#amigo');
+const listaAmigos = document.querySelector('#listaAmigos');
+let amigos = [];
+
+function adicionarAmigo() {
+    const nome = amigo.value.trim();
+
+    if (!nome) {
+        alert("Digite um nome !!!");
+        return;
+    }else {
+        amigos.push(nome);
+        amigo.value = "";
+        atualizarLista();
+    }
+}
+
+function atualizarLista() {
+    listaAmigos.innerHTML = "";
+    amigos.forEach(nome => {
+        const li = document.createElement("li");
+        li.textContent = nome;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if(amigos.length == 0){
+        alert("Nenhum amigo para ser sorteado")
+    }
+
+     const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+
+}
